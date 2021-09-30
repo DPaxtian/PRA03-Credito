71d830756ef589b8d9b4f57fda8b49b1f8de8fe9
Daniel Paxtian <daniel.paxtian98@hotmail.com>
2021-09-29 21:32:18 -0500

    Integracion del metodo para mostrar la interfaz

--- PRA03-Credito/src/pra03/credito/PRA03Credito.java
+++ PRA03-Credito/src/pra03/credito/PRA03Credito.java
@@ -1,13 +1,27 @@
 package pra03.credito;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class PRA03Credito extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(PRA03Credito.class.getResource("CreditoView.fxml"));
+            Pane ventana = (Pane) loader.load();
+            
+            Scene scene = new Scene(ventana);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch(IOException e){
+            System.out.println(e);
+        }
     }
 
     public static void main(String[] args) {
