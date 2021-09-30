ca900662a393613f0c0e56848520206cac3feb15
Daniel Paxtian <daniel.paxtian98@hotmail.com>
2021-09-29 22:59:25 -0500

    Termino de diseño de la interfaz

--- PRA03-Credito/src/pra03/credito/CreditoViewController.java
+++ PRA03-Credito/src/pra03/credito/CreditoViewController.java
@@ -1,27 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pra03.credito;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
-/**
- * FXML Controller class
- *
- * @author daniel
- */
+
 public class CreditoViewController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private ToggleGroup tgIngresos;
+    @FXML
+    private ToggleGroup tgNegocio;
+    @FXML
+    private ToggleGroup tgProducto;
+    @FXML
+    private Button btnCalcular;
+    @FXML
+    private RadioButton rbRango1;
+    @FXML
+    private RadioButton rbRango2;
+    @FXML
+    private RadioButton rbRango3;
+    @FXML
+    private RadioButton rbRango4;
+    @FXML
+    private RadioButton rbFijo;
+    @FXML
+    private RadioButton rbVtaDirecta;
+    @FXML
+    private RadioButton rbSemifijo;
+    @FXML
+    private RadioButton rbMovil;
+    @FXML
+    private RadioButton rbModa;
+    @FXML
+    private RadioButton rbAbarrotes;
+    @FXML
+    private RadioButton rbAlimentos;
+    @FXML
+    private RadioButton rbLimpieza;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       
     }    
+
+    @FXML
+    private void calcularCredito(ActionEvent event) {
+    }
     
 }
